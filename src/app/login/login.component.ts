8c293adaad05bf5611f2371d58618f6d90aca946
abdo-saleh-chatbotech <mea.abdo.saleh@chatbotech.com>
2020-11-13 11:21:08 +0100

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-  
-  ngOnInit(): void {
-  }
+  form: FormGroup
 
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.form = new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl(''),
+    });
+
+
+  }
+  submit() {
+    if (this.form.valid) {
+      this.submitEM.emit(this.form.value);
+    }
+  }
+  @Output() submitEM = new EventEmitter();
 }
